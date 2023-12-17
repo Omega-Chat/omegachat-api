2f5410347a94d44c3a78ed21ce0154a0c5b2043c
Guilherme Correia Tapajós Araújo <90109601+Gtapajos@users.noreply.github.com>
2023-12-17 13:54:26 -0400

    login route and interface modifications

--- services/db_connection/user.ts
+++ services/db_connection/user.ts
@@ -2,24 +2,32 @@ import mongoose, { Schema } from 'mongoose';
 import { DB_URL } from '../../constants.ts';
 import { Connection } from '../connection.ts';
 
-interface ChatUser {
-  email: string
-  name: string
-  password: string
-  pub_key: number[]
-  id_addressee?: string
-  id_group?: string
+interface ElGamalPublicKey {
+  p: bigint;
+  g: bigint;
+  e: bigint;
 }
 
-const ChatUserSchema = new Schema<ChatUser>(
+interface User {
+  _id?: string,
+  email: string,
+  name: string,
+  password: string,
+  pub_key: ElGamalPublicKey,
+  id_addressee?: string,
+  id_group?: string,
+  online: boolean
+}
+
+const UserSchema = new Schema<User>(
   {
     email: String,
     name: String,
     password: String,
-    pub_key: [Number],
+    pub_key: {p: String, g: String, e: String},
     id_addressee: {
       type: String,
-      ref: 'ChatUser',
+      ref: 'User',
     },
     id_group: {
       type: String,
@@ -29,9 +37,9 @@ const ChatUserSchema = new Schema<ChatUser>(
   { timestamps: true }
 );
 
-const ChatUserModel = mongoose.model('ChatUser', ChatUserSchema);
+const UserModel = mongoose.model('User', UserSchema);
 
-export class MongoDBChatUserService {
+export class MongoDBUserService {
   constructor(private readonly connection: Connection) {}
 
   async connect(): Promise<void> {
@@ -40,24 +48,60 @@ export class MongoDBChatUserService {
     if (!isConnected) throw new Error('DB not connected');
   }
 
-  async create(newUser: ChatUser): Promise<ChatUser> {
+  async create(newUser: User): Promise<User> {
     await this.connect();
-    const createdUser = await ChatUserModel.create(newUser);
+    const createdUser = await UserModel.create(newUser);
     return createdUser;
   }
 
-  async findById(userId: string): Promise<ChatUser | null | undefined> {
+  async findById(userId: string): Promise<User | null | undefined> {
     await this.connect();
-    return ChatUserModel.findById(userId);
+    return UserModel.findById(userId);
   }
 
-  async findAll(): Promise<ChatUser[]> {
+  async findAll(): Promise<User[]> {
     await this.connect();
-    return ChatUserModel.find({});
+    return UserModel.find({});
   }
 
   async deleteById(userId: string): Promise<void> {
     await this.connect();
-    await ChatUserModel.deleteOne({ _id: userId });
+    await UserModel.deleteOne({ _id: userId });
   }
+
+  async updatePubKey(userId: string, newPubKey: number[]): Promise<User | null> {
+    try {
+      await this.connect();
+      const updatedUser = await UserModel.findByIdAndUpdate(
+        userId,
+        { $set: { pub_key: newPubKey } },
+        { new: true }
+      );
+      return updatedUser;
+    } catch (error) {
+      console.error('Error updating pub_key:', error);
+      return null;
+    }
+  }
+  
+  async updateAddressId(userId: string, newAddressId: string): Promise<User | null> {
+    try {
+      await this.connect();
+      const updatedUser = await UserModel.findByIdAndUpdate(
+        userId,
+        { $set: { id_addressee: newAddressId } },
+        { new: true }
+      );
+      return updatedUser;
+    } catch (error) {
+      console.error('Error updating id_addressee:', error);
+      return null;
+    }
+  }
+
+  async findUserByEmailAndPassword(email: string, password: string): Promise<User | null> {
+    await this.connect();
+    return UserModel.findOne({ email, password });
+  }
+  
 }
--- ws_routes/user.ts
+++ ws_routes/user.ts
@@ -1,17 +1,17 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import { MongoDBConnection } from '../services/db_connection/mdb_connection.ts';
-import { MongoDBChatUserService } from '../services/db_connection/user.ts';
+import { MongoDBUserService } from '../services/db_connection/user.ts';
 
 const UserRouter = express.Router();
 
-const chatUserService = new MongoDBChatUserService(new MongoDBConnection());
+const UserService = new MongoDBUserService(new MongoDBConnection());
 
 // Route to create a new user
 UserRouter.post('/users', async (req: Request, res: Response) => {
   try {
     const newUser = req.body; // Assuming the request body contains user data
-    const createdUser = await chatUserService.create(newUser);
+    const createdUser = await UserService.create(newUser);
     res.status(201).json(createdUser);
   } catch (error: any) {
     res.status(400).json({ message: error?.message || 'Error creating a new user' });
@@ -22,7 +22,7 @@ UserRouter.post('/users', async (req: Request, res: Response) => {
 UserRouter.get('/users/:userId', async (req: Request, res: Response) => {
   try {
     const { userId } = req.params;
-    const user = await chatUserService.findById(userId);
+    const user = await UserService.findById(userId);
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
@@ -35,7 +35,7 @@ UserRouter.get('/users/:userId', async (req: Request, res: Response) => {
 // Route to get all users
 UserRouter.get('/users', async (_req: Request, res: Response) => {
   try {
-    const allUsers = await chatUserService.findAll();
+    const allUsers = await UserService.findAll();
     res.json(allUsers);
   } catch (error: any) {
     res.status(400).json({ message: error?.message || 'Error fetching users' });
@@ -46,10 +46,59 @@ UserRouter.get('/users', async (_req: Request, res: Response) => {
 UserRouter.delete('/users/:userId', async (req: Request, res: Response) => {
   try {
     const { userId } = req.params;
-    await chatUserService.deleteById(userId);
+    await UserService.deleteById(userId);
     res.json({ message: 'User deleted successfully' });
   } catch (error: any) {
     res.status(400).json({ message: error?.message || 'Error deleting user' });
+  }
+});
+
+// Route to update pub_key of a user
+UserRouter.put('/users/:userId/pub_key', async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { pub_key } = req.body;
+    const updatedUser = await UserService.updatePubKey(userId, pub_key);
+    
+    if (updatedUser) {
+      res.status(200).json(updatedUser);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating pub_key' });
+  }
+});
+
+// Route to update id_addressee of a user
+UserRouter.put('/users/:userId/id_addressee', async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { id_addressee } = req.body;
+    const updatedUser = await UserService.updateAddressId(userId, id_addressee);
+    
+    if (updatedUser) {
+      res.status(200).json(updatedUser);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating id_addressee' });
+  }
+});
+
+// Route to find a user by email and password
+UserRouter.get('/users/:email/:password', async (req, res) => {
+  try {
+    const { email, password } = req.params;
+    const user = await UserService.findUserByEmailAndPassword(email, password);
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error finding user' });
   }
 });
 
